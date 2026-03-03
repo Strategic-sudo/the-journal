e70b3a82401d43718214f20a51c868d1a64d2ec7
Security-sudo <pavlositpro@gmail.com>
2026-03-03 22:52:00 +0200

    Update index.js

--- worker/src/index.js
+++ worker/src/index.js
@@ -3,8 +3,15 @@ const app = new Hono()
 
 
 app.get("/api/posts/:slug", async (c) => {
-  // Do something and return an HTTP response
-  // Optionally, do something with `c.req.param("slug")`
+  const { slug } = c.req.param();
+  const { results } = await c.env.DB.prepare(
+    `
+    select * from posts where id = ?
+  `,
+  )
+    .bind(slug)
+    .run();
+  return c.json(results);
 });
 
 app.post("/api/posts/:slug", async (c) => {
