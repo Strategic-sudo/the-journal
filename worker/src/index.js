dd0d27a2ed143cd7bd8ddd49c0b3364434c1a209
Security-sudo <pavlositpro@gmail.com>
2026-03-03 22:45:56 +0200

    Update index.js

--- worker/src/index.js
+++ worker/src/index.js
@@ -1,6 +1,20 @@
 import { Hono } from 'hono'
 const app = new Hono()
 
+
+app.get("/api/posts/:slug", async (c) => {
+  // Do something and return an HTTP response
+  // Optionally, do something with `c.req.param("slug")`
+});
+
+app.post("/api/posts/:slug", async (c) => {
+  // Do something and return an HTTP response
+  // Optionally, do something with `c.req.param("slug")`
+});
+
+
 app.get('/', (c) => c.text('Hono!'))
+
+
 
 export default app
